0dc754fc2e983195dd3bab8f963ca4780831a9e1
Alexandra Pestruyeva <apestruy@gmail.com>
2020-01-06 05:13:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,12 @@ class Application
       	if item = @@items.find{|i| i.name == item_name}
       		     resp.write item.price
       	else
-      	  
-      	  
-      	  
+      	  resp.status = 400
+          resp.write "Item not found"
+      	end 
+    else 
+      
+      
     		end
  
     		resp.finish
