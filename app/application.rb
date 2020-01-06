346d9a4e889e97d68714506637d460ca6d9f1e0a
Alexandra Pestruyeva <apestruy@gmail.com>
2020-01-06 05:09:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,14 @@
 class Application
-  	     @@songs = [Song.new("Sorry", "Justin Bieber"),
-            		Song.new("Hello","Adele")]
+  	    @@items = []
  
-  	     def call(env)
-    		resp = Rack::Response.new
-    		req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+		req = Rack::Request.new(env)
  
-    		if req.path.match(/songs/)
-      		     song_title = req.path.split("/songs/").last 
+    if req.path.match(/songs/)
+      	song_title = req.path.split("/songs/").last 
 				#turn /songs/Sorry into Sorry
-      		     song = @@songs.find{|s| s.title == song_title}
+      	song = @@songs.find{|s| s.title == song_title}
  
       		     resp.write song.artist
     		end
